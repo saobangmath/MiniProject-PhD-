56db97a5ff4f3c0b6e3dbb5f71d4f7e2a4a5d001
saobangmath <anhtai2672000@gmail.com>
2026-08-05 18:23:15 +0800

    add more experiements

--- MCMC/libs/smc.py
+++ MCMC/libs/smc.py
@@ -24,8 +24,8 @@ class SMC:
         self.__key = key
 
     def _split_key(self):
-        self.__key, subkey = random.split(self.__key)
-        return subkey
+        self.__key, sub_key = random.split(self.__key)
+        return sub_key
 
     def _base_logpdf(self, x):
         if self.__prior_dist_logpdf:
@@ -108,7 +108,7 @@ class SMC:
         weight = jnp.exp(log_w - jax.nn.logsumexp(log_w))
 
         # log(z_(i+1)) - log(z_i) = log(1 / N * sigma weight)
-        diff_log_z = jax.nn.logsumexp(log_w) - jnp.log(len(weight)) 
+        diff_log_z = jax.nn.logsumexp(log_w) - jnp.log(samples.shape[0])
 
         key, k_resample, k_mcmc = random.split(key, 3)
         n = samples.shape[0]
